c3e4d14121a6185d5b3fa0f07e391ae8b64f73b1
tanxianjing <xj1079139185@126.com>
2018-02-09 10:57:50 +0800

    视图圆角

--- Example/TXJAllTools/TXJViewController.m
+++ Example/TXJAllTools/TXJViewController.m
@@ -21,13 +21,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    UIButton *btn = [UIButton buttonWithTitle:@"这是个测试" buttonTitleFontSize:14.0 buttonTitleColor:[UIColor colorWithHexString:@"F85753"] buttonBgColor:nil buttonTextAlignment:NSTextAlignmentCenter];
+    UIButton *btn = [UIButton buttonWithTitle:@"这是个测试" buttonTitleFontSize:14.0 buttonTitleColor:[UIColor colorWithHexString:@"F85753"] buttonBgColor:[UIColor redColor] buttonTextAlignment:NSTextAlignmentCenter];
     
     btn.frame = CGRectMake(20,100 ,self.view.bounds.size.width-40, 44);
-    [btn buttonGradient:@[[UIColor redColor],[UIColor blueColor]] buttonCGSize:btn.bounds.size ByGradientType:TXJDirectionTypeLeft2Right cornerRadius:YES];
+//    [btn buttonGradient:@[[UIColor redColor],[UIColor blueColor]] buttonCGSize:btn.bounds.size ByGradientType:TXJDirectionTypeLeft2Right cornerRadius:YES];
     [self.view addSubview:btn];
     
-    
+    [btn addRoundedCorners:UIRectCornerTopLeft|UIRectCornerTopRight withRadii:CGSizeMake(22, 22)];
 
 }
 
