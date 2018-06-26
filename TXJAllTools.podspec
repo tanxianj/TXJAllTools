066c787a981460fb8ef0fcfeb3a374a96c80b505
谭显敬 <xj1079139185@126.com>
2018-06-26 16:55:12 +0800

    0.1.39更新

--- TXJAllTools.podspec
+++ TXJAllTools.podspec
@@ -8,8 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TXJAllTools'
-  s.version          = '0.1.38'
-  s.summary          = 'this is TP'
+  s.version          = '0.1.39'
+  s.summary          = 'this is TPZ'Z
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
--- TXJAllTools/Classes/AllTools.h
+++ TXJAllTools/Classes/AllTools.h
@@ -21,18 +21,13 @@
 #import "UIView+LSCore.h"
 #endif /* AllTools_h */
 
-#ifdef DEBUG
-#define NSLog(FORMAT, ...)  fprintf(stderr,"<%s %d>:%s\n",[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String])
-#else
-#define NSLog(FORMAT, ...)  nil
-#endif
 // 自定义log输出，debug时，正常NSLog输出，release状态，为空，不打印
 #ifdef DEBUG // 调试时DEBUG
 
-#define DeBuGLog(...) NSLog(__VA_ARGS__)
+#define DeBuGLog(FORMAT, ...)  fprintf(stderr,"<%s %d>:%s\n",[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String])
 
 #else // 发布时
 
-#define DeBuGLog(...)
+#define DeBuGLog(FORMAT, ...) nil
 
 #endif
